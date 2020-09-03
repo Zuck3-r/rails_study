8908b135a2083cdb4dfdd706ed0473dc2edc5710
Ubuntu <ubuntu@ip-172-31-63-85.ec2.internal>
2020-09-03 00:24:30 +0000

    update

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_08_27_151224) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
