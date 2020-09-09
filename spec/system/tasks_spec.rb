952f35e3f2fd5e19936bb3ca2e74ff48b10de973
Ubuntu <ubuntu@ip-172-31-63-85.ec2.internal>
2020-09-09 04:21:30 +0000

    update

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -3,15 +3,19 @@ require 'rails_heper'
 describe 'タスク管理機能', type: :system do
     describe '一覧表示' do
         before do
-            #brhubrhu
+            user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
+            FactoryBot.create(:task, name: '最初のタスク', user: user_a)
         end
         context 'ユーザーAがログインしているとき' do
             before do
-                #User A login
+                visit login_path
+                fill_in 'メールアドレス', with: 'a@example.com'
+                fill_in 'パスワード', with: 'password'
+                click_button 'ログインする'
             end
             
             it 'ユーザーAが作成したタスクが表示される' do
-                #作成済みのタスクの名称が画面上に表示されていることを確認
+                expect(page).to have_content '最初のタスク'
             end
         end
     end
